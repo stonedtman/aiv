65e3b996cf42a9820d9bac75cf4c42e8072c840e
Li <vocaloidliu@qq.com>
2025-02-20 10:41:57 +0800

    修复 rabbitm_utils.py 中变量名和参数问题

--- ai_video_python/rabbitmq/rabbitmq_utils.py
+++ ai_video_python/rabbitmq/rabbitmq_utils.py
@@ -2,13 +2,18 @@ import pika
 import json
 from utils.yamlConfig import read_config
 
-config=read_config()
+config = read_config()
+
+
 def send_to_rabbitmq(queue_name, message):
-    minio_config = config['rabbitmq']
-    credentials = pika.PlainCredentials(minio_config['username'],minio_config['password'])
+    rabbitmq_config = config["rabbitmq"]
+    credentials = pika.PlainCredentials(
+        rabbitmq_config["username"], rabbitmq_config["password"]
+    )
     parameters = pika.ConnectionParameters(
-        host=minio_config['host'],
-        credentials=credentials
+        host=rabbitmq_config["host"],
+        port=rabbitmq_config["port"],
+        credentials=credentials,
     )
 
     try:
@@ -16,9 +21,7 @@ def send_to_rabbitmq(queue_name, message):
         channel = connection.channel()
         channel.queue_declare(queue=queue_name, durable=True)
         channel.basic_publish(
-            exchange='',
-            routing_key=queue_name,
-            body=json.dumps(message)
+            exchange="", routing_key=queue_name, body=json.dumps(message)
         )
         print(f"发送 '{json.dumps(message)}'")
         connection.close()
